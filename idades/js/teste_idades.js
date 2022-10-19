7cc45320ffaa95a8466fdf3426f2e35714152424
vtfeitosa <vfmidia@hotmail.com>
2022-10-19 01:12:16 -0300

    chore: add subtitle to #media

--- idades/js/teste_idades.js
+++ idades/js/teste_idades.js
@@ -1,3 +1,5 @@
+"use strict"
+
 let age_p1 = document.querySelector('#age_1');
 let age_p2 = document.querySelector('#age_2');
 let age_p3 = document.querySelector('#age_3');
@@ -7,6 +9,6 @@ let media = document.querySelector("#media");
 function averageAge(){
     let total = (parseInt(age_p1.value) + parseInt(age_p2.value) + parseInt(age_p3.value));
 
-    media.innerHTML = ((total)/3);
+    media.innerHTML = ("A média de idades das 3 pessoas é: " + ((total)/3));
  
 }

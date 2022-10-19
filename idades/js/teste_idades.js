ee6c77514969778d684cae02747b9e5b058594bd
vtfeitosa <vfmidia@hotmail.com>
2022-10-19 02:55:37 -0300

    fix: add Math.round at #media

--- idades/js/teste_idades.js
+++ idades/js/teste_idades.js
@@ -9,6 +9,6 @@ let media = document.querySelector("#media");
 function averageAge(){
     let total = (parseInt(age_p1.value) + parseInt(age_p2.value) + parseInt(age_p3.value));
 
-    media.innerHTML = ("A média de idades das 3 pessoas é: " + ((total)/3));
+    media.innerHTML = ("A média de idades das 3 pessoas é: " + (Math.round((total)/3)));
  
 }

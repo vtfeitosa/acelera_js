288a1e5d651459e9a6e3f229c4880ae3974f2d23
vtfeitosa <vfmidia@hotmail.com>
2022-10-19 00:59:06 -0300

    chore: add .js file

---
+++ idades/js/teste_idades.js
@@ -0,0 +1,12 @@
+let age_p1 = document.querySelector('#age_1');
+let age_p2 = document.querySelector('#age_2');
+let age_p3 = document.querySelector('#age_3');
+
+let media = document.querySelector("#media");
+
+function averageAge(){
+    let total = (parseInt(age_p1.value) + parseInt(age_p2.value) + parseInt(age_p3.value));
+
+    media.innerHTML = ((total)/3);
+ 
+}

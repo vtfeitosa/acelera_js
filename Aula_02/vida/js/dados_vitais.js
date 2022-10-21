0e86cc5d019725b8ba63c8abb4bb8adbf7548951
vtfeitosa <vfmidia@hotmail.com>
2022-10-21 06:35:28 -0300

    chore: add some comments

--- Aula_02/vida/js/dados_vitais.js
+++ Aula_02/vida/js/dados_vitais.js
@@ -1,28 +1,39 @@
 "use strict"
 
+//Função para pular linha
 function skipLine(){
     document.write("</br>");
 }
 
+//Função para escrever um determinado conteúdo na página
 function writeContent(content){
     document.write(content);
 }
 
+//Atribuit o valor inserido no prompt ao valor de ageInput
 let ageInput = prompt("Quantos anos você tem?");
 
+//Função para calcular a quantidade de dias vividos
 function daysLivedCalc(age){
+    //O parãmetro age é o valor que foi atribuído na linha 23
     let ageDays = age * 365;
     return ageDays ;
 }
 
 let daysLived = daysLivedCalc(ageInput);
+//Essa variável daysLived recebe o resultado da função(linha20),
+//...e usou como parâmetro para esse cálculo, o agrInput(linha14)
 
-function heartRateCalc(rate){
+//Função para calcular a quantidade de batimentos cardíacos que a
+//...pessoa teve durante sua vida
+function heartRateCalc(){
     let heartRateNum = (daysLived*(24*(60*80)));
+    //essa função não pegou parâmentro pq a informação já estava no geral
     return heartRateNum ;
+    //esse retorno é atribuído á variavel nomeada heartRate(linha36)
 }
 
-let heartRate = heartRateCalc()
+let heartRate = heartRateCalc();
 
 writeContent("Você já viveu " + daysLived + " dias de vida!");
 skipLine();

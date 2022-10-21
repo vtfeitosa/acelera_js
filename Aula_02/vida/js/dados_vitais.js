27dbf96da0dff05284f82da28a4e6fe20f7d2a22
vtfeitosa <vfmidia@hotmail.com>
2022-10-21 04:40:41 -0300

    chore: exc 3.6 - base

--- Aula_02/vida/js/dados_vitais.js
+++ Aula_02/vida/js/dados_vitais.js
@@ -7,3 +7,14 @@ function skipLine(){
 function writeContent(content){
     document.write(content);
 }
+
+let age = prompt("Quantos anos você tem?");
+let days = age * 365;
+let heartRate = (days*(24*(60*80)));
+
+writeContent("Você já viveu " + days + " dias de vida!");
+skipLine();
+writeContent("E seu coração já bateu " + heartRate + " vezes. Haja Coração!")
+console.log(heartRate);
+
+

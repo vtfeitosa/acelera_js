0f45a27ac5d7ed29a0688b5f53405e93c53c0c0f
vtfeitosa <vfmidia@hotmail.com>
2022-10-21 02:11:30 -0300

    fix: media get from #media from DOM

--- Aula_01/idades/js/teste_idades.js
+++ Aula_01/idades/js/teste_idades.js
@@ -6,9 +6,17 @@ let age_p3 = document.querySelector('#age_3');
 
 let media = document.querySelector("#media");
 
+function skipLine() {
+    media.innerHTML +=("<hr><br><hr>");
+   }
+
+let Year = 2022;
+
 function averageAge(){
     let total = (parseInt(age_p1.value) + parseInt(age_p2.value) + parseInt(age_p3.value));
 
-    media.innerHTML = ("A média de idades das 3 pessoas é: " + (Math.round((total)/3)));
- 
+    media.innerHTML = ("A média de idades das 3 pessoas é: ");
+    skipLine();
+    media.innerHTML += (Math.round((total)/3));
+
 }

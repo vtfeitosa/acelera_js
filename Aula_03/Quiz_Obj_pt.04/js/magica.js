d63e2a42ffeaa49e6eb58682206c95c1328e39e4
vtfeitosa <vfmidia@hotmail.com>
2022-10-22 20:50:13 -0300

    code upgrade: Projetinho - Adiciona Texto

--- Aula_03/Quiz_Obj_pt.04/js/magica.js
+++ Aula_03/Quiz_Obj_pt.04/js/magica.js
@@ -6,9 +6,16 @@ function skipLine(){
 }
 
 //Função para escrever um determinado conteúdo na página
-function writeContent(content){
-    let box = document.querySelector("body main");
+function writePageContent(content){
+    let box = document.querySelector("#pageContent");
     box.innerHTML = (content);
 }
 
-writeContent(`<h1>Minha página, modificada por JavaScript</h1> <input placeholder="TESTE" ></input>`);
+writePageContent(`<div><h1>Minha página, modificada por JavaScript</h1><p id= contentFromInput ></p></div><div><input id= inputContent placeholder="teste" ></input><button id=btn onclick=" writeFromInput()" >Adicionar</button>`);
+
+
+function writeFromInput(){
+    let inputContent = document.getElementById("inputContent");
+    let contentFromInput = document.querySelector("#contentFromInput");
+    contentFromInput.innerHTML += (inputContent.value);    
+}

a40175fb739389843aeb352ffeea9bcef664275c
vtfeitosa <vfmidia@hotmail.com>
2022-10-23 20:43:41 -0300

    chore: add actions to btnLivros and some code coments

---
+++ Projeto_Final/scripts/index.js
@@ -0,0 +1,27 @@
+import { btnActive} from "./activateButton.js";
+
+//Captura os elementos do dom para o innerHTML
+const btnNotes = document.querySelector(".link-pagina-notas");
+const btnLivros = document.querySelector(".link-pagina-livros");
+const btnEdit = document.querySelector("section nav button:nth-of-type(3)");
+const btnTrash = document.querySelector(".link-pagina-lixeira");
+
+//Selecionando os botões e atribuindo a cada um, o evento "click" para executar
+//...gerar uma variável que será o parâmetro de funções executadas a seguir.
+btnNotes.addEventListener("click" , ()=>{
+    btnSelected("notes");
+} );
+btnLivros.addEventListener("click" , ()=>{
+    btnSelected("books");
+});
+btnEdit.addEventListener("click" , ()=>{
+    btnSelected("edit");
+});
+btnTrash.addEventListener("click" , ()=>{
+    btnSelected("trash");
+});
+
+//Quando o botão for selecionado, executar a função de "ativação" em scripts/activateButton.js(linha33)
+function btnSelected(btnName){
+    btnActive(btnName);
+}

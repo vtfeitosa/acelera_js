ad099f02d1db699b50801b0c5b2d929b2d0fb84f
vtfeitosa <vfmidia@hotmail.com>
2022-10-25 12:59:57 -0300

    some coments

--- Aula_05/jogo/js/index.js
+++ Aula_05/jogo/js/index.js
@@ -15,15 +15,21 @@ function writePageContent(content){
 
 //Sorteando um número aleatório
 let numberDrawn = (Math.round((Math.random())*100));
-//na linha acima está arredondando o número aleatório q veio entre 
+//na linha acima está arredondando o número aleatório q veio entre 0.1 e 1
+//... multiplicando por 100, tornando o número sorteado um numero inteiro entre 10 e 100
+//...pq o Math.round arredonda para um número inteiro
 console.log (numberDrawn);
 
+//Capturando o input do número escolhido pelo jogador para fazer a comparação
 let playerNumber = prompt("Já pensei. Qual você acha que é? (Entre 0 e 100)");
 
+//Compara o número sorteado pela máquina e o número escolhido pelo usuário
 if(playerNumber == numberDrawn){
+    //Se o jogador acertar o número que a máquina sorteou
     writePageContent("Uau! Você acertou, eu também pensei no número " + numberDrawn);
 
 }else{
+    //Se o jogador errar o número que a máquina sorteou
     writePageContent("Você errou! Eu tinha pensado no " + numberDrawn);
 
 }

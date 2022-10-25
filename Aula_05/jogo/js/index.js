d69a5cc925d8209636b1436ed1e0c408ee84c077
vtfeitosa <vfmidia@hotmail.com>
2022-10-25 14:00:25 -0300

    add exc 4.6 - q.2

--- Aula_05/jogo/js/index.js
+++ Aula_05/jogo/js/index.js
@@ -13,31 +13,36 @@ function writePageContent(content){
     box.innerHTML += (content);
 }
 
-//Sorteando um número aleatório
-let numberDrawn = (Math.round((Math.random())*100));
-//na linha acima está arredondando o número aleatório q veio entre 0.1 e 1
-//... multiplicando por 100, tornando o número sorteado um numero inteiro entre 10 e 100
-//...pq o Math.round arredonda para um número inteiro
-console.log (numberDrawn);
+//Mltiplicando por 100, tornando o número sorteado um numero inteiro entre 1 e 100
+let numSorteadoInt = sorteio(100);
+
+//Sorteando um número aleatório e transformando em inteiro com o uso do parâmetro n
+function sorteio(n){
+let numSorteado = Math.round((Math.random()* n ));
+//na linha acima está arredondando o número aleatório q veio entre 0.1 e 1 pq o Math.round arredonda 
+//...para um número inteiro pq multiplica por 100 q é o parâmetro usado como valor de n
+console.log (numSorteado);
+return numSorteado;
+}
 
 //Capturando o input do número escolhido pelo jogador para fazer a comparação
 let playerNumber = prompt("Já pensei. Qual você acha que é? (Entre 0 e 100)");
 
 //Compara o número sorteado pela máquina e o número escolhido pelo usuário
-if(playerNumber == numberDrawn){
+if(playerNumber == numSorteadoInt){
     //Se o jogador acertar o número que a máquina sorteou
-    writePageContent("Uau! Você acertou, eu também pensei no número " + numberDrawn);
+    writePageContent("Uau! Você acertou, eu também pensei no número " + numSorteadoInt);
 
-}else if(playerNumber != numberDrawn){
+}else if(playerNumber != numSorteadoInt){
     //Se o jogador errar o número que a máquina sorteou
-    writePageContent("Você errou! Eu tinha pensado no " + numberDrawn);
+    writePageContent("Você errou! Eu tinha pensado no " + numSorteadoInt);
 
     //Vai ser feita outra comparação se era > ou < 
-    if(playerNumber < numberDrawn){
+    if(playerNumber < numSorteadoInt){
         skipLine();
         writePageContent( "O número que eu sorteei era MAIOR que o escolhido por você!");
 
-    }else if(playerNumber > numberDrawn){
+    }else if(playerNumber > numSorteadoInt){
         skipLine();
         writePageContent( "O número que eu sorteei era MENOR que o escolhido por você!");
     }   

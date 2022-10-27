bc208264b64ad12b3657c28946561a8a3e690f74
vtfeitosa <vfmidia@hotmail.com>
2022-10-27 05:36:09 -0300

    chore: code upgrade - generating the page with .js file

--- Aula_07/consumo/js/consumo.js
+++ Aula_07/consumo/js/consumo.js
@@ -1,36 +1,85 @@
 "use strict"
 
-let fuelType = document.querySelector("#fuelType");
-let alc = document.querySelector("#alc");
-let gas = document.querySelector("#gas");
+//Elemento do DOM no qual o conteúdo será impresso
+let contentBox = document.querySelector("#pageMainContent");
 
+//Função para pular linha
+function skipLine(){
+    result.innerHTML += ("</br>");
+}
+
+//Função para escrever um determinado conteúdo na página
+function writePageContent(content){
+    contentBox.innerHTML += (content);
+}
+
+//Capturando elementos do DOM para "imprimir" o header e footer
+let headerBox = document.querySelector(".development");
+let footerBox = document.querySelector("footer");
+
+//Header
+headerBox.innerHTML = (`
+<p>Developed by <a href="https://github.com/vtfeitosa" target="_blank">vtfeitosa</a></p>
+`);
+
+//Footer
+footerBox.innerHTML = (`
+<section class="footer_extra">
+<p>So this page was developed as an activity of WEB Development Trail at Alpha EdTech, by the student <a href="https://www.linkedin.com/in/vtfeitosa/" target="_blank">Vitória Feitosa</a>.<br/> All the code is being studied, patience... Thank you for understanding. :) </p>
+<p>Last Update on 27 | October | 2022 </p>
+</section>
+`);
+
+//////////////
+
+writePageContent(`
+<h1>Álcool ou Gasolina?</h1>
+<p>Se você ainda fica na dúvida sobre o consumo de combustíveldo seu veículo, faça já o teste! </br>Preencha os dados, e verifique qual o nível do consumo de acordo com o combustível usado.</p>
+<div id="calcBox">
+    <div id="fuelBox">
+        <select id="fuelType">
+            <option selected disabled>-- Selecione um Combustível --</option>
+            <option value="Álcool">Álcool</option>
+            <option value="Gasolina">Gasolina</option>
+        </select>
+    </div>
+    <div id="priceBox">
+        <label>Valor do Litro de Combustível:<input id="fuelPrice" placeholder="BRL - Reais"></label>
+    </div>
+    <div id="pathBox">
+        <label>Percurso percorrido:<input id="path" placeholder="Km"></label>
+    </div>
+    <div id="tankBox">
+        <label>Capacidade do Tanque:<input id="tankCapacity" placeholder="Litros"></label>
+    </div>
+    <div id="btnBox">
+        <button id="btn" onclick="fuelConsumption()">Calcular</button>
+    </div>
+</div>
+<p id="result"></p>
+`);
+
+//////////////
+
+//Capturando Inputs
+let fuelType = document.querySelector("#fuelType");
 let path = document.querySelector("#path");
 let tankCapacity = document.querySelector("#tankCapacity");
-
 let fuelPrice = document.querySelector("#fuelPrice");
 
-consumption = document.querySelector("#consumption");
-
-function skipLine() {
-    consumption.innerHTML +=("<br>");
-   }   
+//Elemento do dom onde vai ser "impresso" o resultado
+let result = document.querySelector("#result");
    
 function fuelConsumption(){
 
-    let consumptionL = (parseInt(path.value)/parseInt(tankCapacity.value));
-    
-    let pathTraveled = (consumptionL).toFixed(2);
+    //Calcula o consumo em litros pelo trajeto percorrido
+    let pathTraveled = ((parseInt(path.value)/parseInt(tankCapacity.value)).toFixed(2));
+    let pathPrice = ((tankCapacity.value) * parseInt(fuelPrice.value));
+    let kmPrice = (((pathPrice)/path.value).toFixed(2));
 
-    let pathPrice = ((tankCapacity.value) * parseFloat(fuelPrice.value));
-
-    let kmPrice = ((pathPrice)/path.value);
-
-    console.log(kmPrice);
-
-    consumption.innerHTML = ("Seu veículo percorre cerca de " + pathTraveled + " Km/Litro de " + fuelType.value + " consumido.");
+    result.innerHTML += ("Seu veículo percorre cerca de " + pathTraveled + " Km/Litro de " + fuelType.value + " consumido.");
     skipLine();
-    consumption.innerHTML += ("O valor gasto de combustível com esse trajeto é R$" + pathPrice + " reais.");
+    result.innerHTML += ("O valor gasto de combustível com esse trajeto é R$" + pathPrice + " reais.");
     skipLine();
-    consumption.innerHTML += ("Abastecendo seu veículo com " + fuelType.value + " você gasta cerca de R$" + kmPrice + " reais por Km percorrido.");
-
+    result.innerHTML += "Abastecendo seu veículo com " + fuelType.value + " você gasta cerca de R$" + kmPrice + " por Km percorrido.";
 }

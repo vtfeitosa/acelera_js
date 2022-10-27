0b6208bbcffbfc2473dd3dd8ffcf74ef1b5755cb
vtfeitosa <vfmidia@hotmail.com>
2022-10-27 01:25:00 -0300

    rmv console.log

--- Aula_06/usados/js/index.js
+++ Aula_06/usados/js/index.js
@@ -37,24 +37,17 @@ function calcPrice() {
         result.innerText = "ERRO! Insira números válidos nos campos acima.";
 
     }else {
-        console.log("é um numero valido");
 
         if (carAge.value < 3) {
-            console.log("menos de 3 anos");
             let salePrice = (purchasePrice.value * 0.8);
-            console.log(salePrice);
             result.innerText = `O preço de venda do seu carro usado é: ${Math.round(salePrice)}`
 
         } if ((carAge.value >= 3) && (carAge.value < 10)) {
-            console.log("entre 3 e 10 anos");
             let salePrice = (purchasePrice.value * 0.7);
-            console.log(salePrice);
             result.innerText = `O preço de venda do seu carro usado é: ${Math.round(salePrice)}`
 
         } if (carAge.value >= 10) {
-            console.log("mais de 10 anos");
             let salePrice = (purchasePrice.value * 0.5);
-            console.log(salePrice);
             result.innerText = `O preço de venda do seu carro usado é: ${Math.round(salePrice)}`
         }
     }

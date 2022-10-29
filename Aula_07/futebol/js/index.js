b1021d35c09fd75e56de54b543f17477dd059923
vtfeitosa <vfmidia@hotmail.com>
2022-10-29 01:38:52 -0300

    Tarefa 4

--- Aula_07/futebol/js/index.js
+++ Aula_07/futebol/js/index.js
@@ -1,53 +1,83 @@
-"use strict"
-let box = document.querySelector("#pageContent");
+"use strict";
+//Elemento do DOM no qual o conteúdo será impresso
+const contentBox = document.querySelector("#pageMainContent");
 
 //Função para pular linha
 
-function skipLine(){
-    box.innerHTML += ("</br>");
+function skipLine() {
+  contentBox.innerHTML += "</br>";
 }
 
 //Função para escrever um determinado conteúdo na página
-function writePageContent(content){
-    box.innerHTML += (content);
+function writePageContent(content) {
+  contentBox.innerHTML += content;
 }
 
-let vitoriasInp = prompt("Quantos jogos o seu time venceu?");
-let empatesInp = prompt("Quantos jogos o seu time empatou?");
-let derrotasInp = prompt("E quantos o seu time perdeu?");
-let vitoriasAdversarioInp = prompt("Quantos jogos o Time Adversário venceu?");
-let empatesAdversarioInp = prompt("Quantos jogos o Time Adversário empatou?");
-let derrotasAdversarioInp = prompt("E quantos o Time Adversário perdeu?")
+writePageContent(`
+    <h1>Pontuação de Partida</h1>
+    <!--Verificar Formulario na div abaixo-->
+    <div">
+        <div class="team1">
+            <h2>Seu Time</h2>
+            <label>Quantos jogos o seu time venceu? <input id="vitoriasInp" type="number"></label>
+            <label>Quantos jogos o seu time empatou? <input id="empatesInp" type="number"></label>
+            <label>E quantos o seu time perdeu? <input id="derrotasInp" type="number"></label>
+        </div>
+        <div class="team2">
+            <h2>Time Adversário</h2>
+            <label>Quantos jogos o Time Adversário venceu? <input id="vitoriasAdversarioInp" type="number"></label>
+            <label>Quantos jogos o Time Adversário empatou? <input id="empatesAdversarioInp" type="number"></label>
+            <label>E quantos o Time Adversário perdeu? <input id="derrotasAdversarioInp" type="number"></label>
+        </div>
+    </div>
+    <button onclick="showResult()">Calcular</button>
+    <p id="result"></p>
+`);
 
+let vitoriasInp = document.querySelector("#vitoriasInp");
+let empatesInp = document.querySelector("#empatesInp");
+let derrotasInp = document.querySelector("#derrotasInp");
+let vitoriasAdversarioInp = document.querySelector("#vitoriasAdversarioInp");
+let empatesAdversarioInp = document.querySelector("#empatesAdversarioInp");
+let derrotasAdversarioInp = document.querySelector("#derrotasAdversarioInp");
 
-//Calculando total de pontos vindos do input de cada time 
+const result = document.querySelector("#result");
 
-function calcPontos(vitorias,empates,derrotas){
-    let totalPontos = (((Number(vitorias) * 3) + Number(empates)) - Number(derrotas) );
-    return totalPontos;
+/////////////////////////////
+
+//Calculando total de pontos vindos do input de cada time
+
+function calcPontos(vitorias, empates, derrotas) {
+  let totalPontos = Number(vitorias) * 3 + Number(empates) - Number(derrotas);
+
+  return totalPontos;
 }
 
-let ptFinal = calcPontos(vitoriasInp,empatesInp,derrotasInp);
-let ptFinalAdv = calcPontos(vitoriasAdversarioInp,empatesAdversarioInp,derrotasAdversarioInp);
-
-
-writePageContent(`Nosso time fez ${ptFinal} pontos!`);
-skipLine();
-writePageContent(`O time adversário fez ${ptFinalAdv} pontos`);
-skipLine();
-writePageContent(comparando(ptFinal , ptFinalAdv));
-
-function comparando(ptFinalNum , ptFinalAdvNum){
-    if (ptFinalNum < ptFinalAdvNum) {
-    return (`Nosso time está indo PIOR que o Time Adversário :(`);
-    
-    }else if (ptFinalNum > ptFinalAdvNum) {
-    return (`Nosso time está indo MELHOR que o Time Adversário hihihi `); 
-    
-    }else if (ptFinalNum== ptFinalAdvNum) {
-    return (`Nosso time está EMPATADO com o Time Adversário `); 
-    }
-
+function comparando(ptFinalNum, ptFinalAdvNum) {
+  if (ptFinalNum < ptFinalAdvNum) {
+    return `Nosso time está indo PIOR que o Time Adversário :(`;
+  } else if (ptFinalNum > ptFinalAdvNum) {
+    return `Nosso time está indo MELHOR que o Time Adversário hihihi `;
+  } else if (ptFinalNum === ptFinalAdvNum) {
+    return `Nosso time está EMPATADO com o Time Adversário `;
+  }
 }
 
+function showResult() {
+  let ptFinal = calcPontos(
+    vitoriasInp.value,
+    empatesInp.value,
+    derrotasInp.value
+  );
+  let ptFinalAdv = calcPontos(
+    vitoriasAdversarioInp.value,
+    empatesAdversarioInp.value,
+    derrotasAdversarioInp.value
+  );
 
+  writePageContent(`Nosso time fez ${ptFinal} pontos!`);
+  skipLine();
+  writePageContent(`O time adversário fez ${ptFinalAdv} pontos`);
+  skipLine();
+  writePageContent(comparando(ptFinal, ptFinalAdv));
+}

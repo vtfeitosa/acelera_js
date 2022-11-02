2e9d7cbb4755327c6ddfae3b8cb3eb8e5958b375
vtfeitosa <vfmidia@hotmail.com>
2022-11-02 02:02:45 -0300

    Atv5.3 - q.1

---
+++ Aula_08/copa/js/index.js
@@ -0,0 +1,44 @@
+"use strict"
+
+//Elemento do DOM no qual o conteúdo será impresso
+const contentBox = document.querySelector("#pageMainContent");
+
+//Função para pular linha
+function skipLine(){
+    contentBox.innerHTML += ("</br>");
+}
+
+//Função para escrever um determinado conteúdo na página
+function writePageContent(content){
+    contentBox.innerHTML += (content);
+}
+
+//Capturando elementos do DOM para "imprimir" o header e footer
+const headerBox = document.querySelector(".development");
+const footerBox = document.querySelector("footer");
+
+//Header
+headerBox.innerHTML = (`
+<p>Developed by <a href="https://github.com/vtfeitosa" target="_blank">vtfeitosa</a></p>
+`);
+
+//Footer
+footerBox.innerHTML = (`
+<section class="footer_extra">
+<p>So this page was developed as an activity of WEB Development Trail at Alpha EdTech, by the student <a href="https://www.linkedin.com/in/vtfeitosa/" target="_blank">Vitória Feitosa</a>.<br/> All the code is being studied, patience... Thank you for understanding. :) </p>
+<p>Last Update on Day | Month | 2022 </p>
+</section>
+`);
+
+////////////////////////////
+
+let anoDeCopa = 1930;
+
+while(anoDeCopa <= 2100){
+    writePageContent(anoDeCopa + " tem copa!");
+    anoDeCopa = anoDeCopa + 4;
+    skipLine();
+}
+
+skipLine();
+writePageContent(`Ufa! Esses foram os anos de copa até 2100.`)

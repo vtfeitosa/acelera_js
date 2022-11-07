0356538b3a3c2144eb64c2cd0205d2d153eba597
vtfeitosa <vfmidia@hotmail.com>
2022-11-07 03:21:36 -0300

    Exc6.2 - q.1e2

--- Aula_11/advinha/js/index.js
+++ Aula_11/advinha/js/index.js
@@ -26,8 +26,31 @@ headerBox.innerHTML = (`
 footerBox.innerHTML = (`
 <section class="footer_extra">
 <p>So this page was developed as an activity of WEB Development Trail at Alpha EdTech, by the student <a href="https://www.linkedin.com/in/vtfeitosa/" target="_blank">Vitória Feitosa</a>.<br/> All the code is being studied, patience... Thank you for understanding. :) </p>
-<p>Last Update on Day | Month | 2022 </p>
+<p>Last Update on 07 | November | 2022 </p>
 </section>
 `);
 
 ////////////////////////////
+
+writePageContent(`
+<label><input type="text" id="num"></input></label>
+<button id="btn" onclick="compare()">Compare com o número que estou pensando!</button>`
+);
+
+let numInp = "";
+let sorteado = "";
+
+function compare(){
+    sorteado = Math.round(Math.random()*100);
+    numInp = document.querySelector("#num");
+    numInp.value = "";
+
+    if(sorteado === (Number(numInp.value))){
+        alert("Parabéns! Você acertou o número secreto");
+
+    }else{
+        alert("Infelizmente você errou!");
+
+    }
+    
+}
